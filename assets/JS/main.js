b154483b4379bb0f45af4f002eec59a7184d37ae
Dierdier13 <didier.piroille@gmail.com>
2025-01-17 13:57:53 +0100

    push pour test

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,17 +1,20 @@
 const banner = document.querySelector('#banner');
 const logo = document.querySelector('#logo');
+const nav = document.querySelector('#nav')
 let lastScrollTop = 0;
 
 window.addEventListener('scroll', () => {
-  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    console.log(lastScrollTop, scrollTop);
   
   if (scrollTop > lastScrollTop) {
     
     // Défilement vers le bas
-    banner.style.height = '100px';
-    logo.style.maxWidth = '150px';
+    banner.style.height = '80px';
+    logo.style.maxWidth = '80px';
     logo.style.position = 'absolute';
-    logo.style.left = '50%';
+    logo.style.left = '60%';
     logo.style.top = '50%';
     logo.style.transform = 'translateY(-50%)';
   } else {
